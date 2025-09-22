e3f9e6292c3bf8a7e4f33081cd710e241743a733
Emukei555 <emukei007@gmail.com>
2025-09-22 21:44:14 +0900

    Delete todoapp directory

--- todoapp/src/main/java/com/example/todoapp/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todoapp.repository;
-
-import com.example.todoapp.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
